9003402b05c07b570083948909108ac71f0b68df
Vahid-Soudagar <mohammadvahidsoudagar264@gmail.com>
2024-10-18 18:46:35 +0530

    changes ina pi

--- app/src/main/java/com/vcreate/ecg/data/service/ApiInterface.kt
+++ app/src/main/java/com/vcreate/ecg/data/service/ApiInterface.kt
@@ -3,11 +3,12 @@ package com.vcreate.ecg.data.service
 import com.vcreate.ecg.data.model.EcgResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ApiInterface {
 
-    @GET("ecg")
+    @POST("ecg")
     suspend fun processEcg(
         @Query("data") data: String
     ): Response<EcgResponse>
