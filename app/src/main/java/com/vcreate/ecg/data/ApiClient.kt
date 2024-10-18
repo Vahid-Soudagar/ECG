d666aadee17e4ce1bada535bf4fb2e572f14e8a2
Vahid-Soudagar <mohammadvahidsoudagar264@gmail.com>
2024-10-18 18:48:21 +0530

    changes ina pi

--- app/src/main/java/com/vcreate/ecg/data/ApiClient.kt
+++ app/src/main/java/com/vcreate/ecg/data/ApiClient.kt
@@ -1,13 +1,23 @@
 package com.vcreate.ecg.data
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiClient {
     private const val BASE_URL = "https://api.bridgehealth.care/v1/"
 
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS) // Set connection timeout
+        .readTimeout(30, TimeUnit.SECONDS) // Set read timeout
+        .writeTimeout(30, TimeUnit.SECONDS) // Set write timeout
+        .build()
+
+
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(client) // Use the custom OkHttpClient
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
